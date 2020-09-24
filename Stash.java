d527d2acab3f5ded5d2e4e6f2fa340f96aced55a
AvishkaWeebadde <senithweebadde@gmail.com>
2020-09-25 04:36:54 +0530

    Addind Stash

--- Stash.java
+++ Stash.java
@@ -1,3 +1,40 @@
 public class Stash{
+	public String getStashID(){
+		return stashID;
 	
+	}
+
+	public String getStashName(){
+		return stashName;
+	}
+
+	public String getStashMessage(){
+		return stashMessage;
+    }
+	
+	public void setStashID(String s){
+		stashID = s;
+	
+	}
+
+	public void getStashName(String a){
+		stashName=a;
+	}
+
+	public void getStashMessage(String b){
+		stashMessage = b;
+    }
+	
+	public void addStash(){
+		
+	}
+	
+	public void removeStash(){
+		
+	}
+	
+	public void clearAllStash(){
+		
+	}
+
 }
